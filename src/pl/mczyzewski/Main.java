ddee751ac3096d57724e760f9a3995c1690de2fe
Michal Czyzewski <czy.mich@gmail.com>
2019-04-24 23:40:09 +0200

    test 2 of increment

--- src/pl/mczyzewski/Main.java
+++ src/pl/mczyzewski/Main.java
@@ -6,12 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.printf("");
+
 
         for (int x=2; x<=8;x++)
         {
             System.out.println("10,000 at " + x +"% intrest = " + String.format("%.2f",ForLoop.calculateInterest(10000,x)));
         }
+
+        for (int x=8; x>=2;x--)
+        {
+            System.out.println("10,000 at " + x +"% intrest = " + String.format("%.2f",ForLoop.calculateInterest(10000,x)));
+        }
+
 
     }
 }
