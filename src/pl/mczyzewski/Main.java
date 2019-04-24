7857a6e381729d80756b8503dac771271e740339
Michal Czyzewski <czy.mich@gmail.com>
2019-04-25 00:18:09 +0200

    test 2 add count

--- src/pl/mczyzewski/Main.java
+++ src/pl/mczyzewski/Main.java
@@ -1,23 +1,61 @@
 package pl.mczyzewski;
 
 
+public class Main
+{
 
-public class Main {
+    public static void main(String[] args)
+    {
+//
+//
+//
+//        for (int x=2; x<=8;x++)
+//        {
+//            System.out.println("10,000 at " + x +"% intrest = " + String.format("%.2f",ForLoop.calculateInterest(10000,x)));
+//        }
+//
+//        System.out.println();
+//
+//        for (int x=8; x>=2;x--)
+//        {
+//            System.out.println("10,000 at " + x +"% intrest = " + String.format("%.2f",ForLoop.calculateInterest(10000,x)));
+//        }
 
-    public static void main(String[] args) {
-
-
-
-        for (int x=2; x<=8;x++)
+        int count = 0;
+        for (int x = 2; x < 13 ; x++)
         {
-            System.out.println("10,000 at " + x +"% intrest = " + String.format("%.2f",ForLoop.calculateInterest(10000,x)));
+            if (isPrime(x))
+            {
+
+                    count++;
+                    System.out.println(x + " is a prime number!");
+                    if(count == 3)
+                    {
+                        break;
+                    }
+
+
+
+
+            }
         }
 
-        for (int x=8; x>=2;x--)
+
+    }
+
+    public static boolean isPrime(int n)
+    {
+        if (n == 1)
         {
-            System.out.println("10,000 at " + x +"% intrest = " + String.format("%.2f",ForLoop.calculateInterest(10000,x)));
+            return false;
         }
-
-
+        for (int i = 2; i <= n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
